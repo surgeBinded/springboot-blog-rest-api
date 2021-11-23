317ef35303f61ea54f0bc4257631beda3f4450e7
surgeBinded <ropotdaniel@gmail.com>
2021-11-23 19:43:41 +0100

    refactor PostService.java

--- src/main/java/com/springbootblog/blog/service/PostService.java
+++ src/main/java/com/springbootblog/blog/service/PostService.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 public interface PostService {
     PostDTO createPost(PostDTO postDTO);
+
     List<PostDTO> getAllPosts();
+
     PostDTO getPostById(Long id);
 
     PostDTO updatePost(PostDTO postDTO, Long id);
